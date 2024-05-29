00490652aeb34efab4f044433ef620b9baf6268d
Pallavi <pallavi25kishore@gmail.com>
2024-05-29 12:44:46 -0700

    implement controller and model for post request

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,4 +1,31 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`);
+
+const glossarySchema = mongoose.Schema({
+id: mongoose.ObjectId,
+text: STRING
+});
+
+const Glossary = mongoose.model('Glossary', glossarySchema);
+
+exports.getAll = function () { // model for get request contoller
+
+
+};
+
+exports.addItem = function (data) { // model for post request contoller
+  return new Glossary(data).save();
+};
+
+exports.updateItem = function () { // model for put request contoller
+
+};
+
+exports.removeItem = function () { // model for delete request contoller
+
+};
+
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const Glossary = require('./db.js');
 
 const app = express();
 
 // Serves up all static and generated assets in in a specified folder.
-app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
-
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.json());
 /****
  *
  *
@@ -15,5 +16,33 @@ app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
  *
  */
 
+app.get('/glossary', function (req, res) {
+
+});
+
+app.post('/glossary', function (req, res) {
+  var data = req.body;
+  Glossary.addItem(data)
+  .then(() => {
+    res.status(201).send('new item saved');
+  })
+  .catch(() => {
+    res.sendStatus(400);
+  })
+});
+
+app.put('/glossary/:id', function (req, res) {
+  var index = req.params.id // NOTE - see how id key is created in mondodb
+
+});
+
+app.delete('/glossary/:id', function (req, res) {
+  var index = req.params.id // NOTE - see how id key is created in mondodb
+
+});
+
+
+
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
+
